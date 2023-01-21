188f1ca1c2be01e7b343f4a1ee690e9d5a6f4fab
tech-yogesh-panchal <yogeshpanchal.techie@gmail.com>
2023-01-21 12:00:00 +0530

    feat(ui): create SignIn and Chat screens with real-time chat functionality
    - Implemented SignIn screen with name and room input validation
    - Integrated query params and Socket.IO connection in Chat screen
    - Setup event handlers for join, sendMessage, and receive message
    - Added dynamic message rendering and user list display

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
-   <div>React App</div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
 }
 
